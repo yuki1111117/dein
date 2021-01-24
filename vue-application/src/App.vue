e5988d07dade691067454227d01bf1a439a01545
Your Name <you@example.com>
2021-01-24 10:37:46 +0900

    add Test.vue component

--- vue-application/src/App.vue
+++ vue-application/src/App.vue
@@ -1,19 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <Test msg="TEST TEST" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import Test from "./components/Test.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+    Test,
+  },
+};
 </script>
 
 <style>
---
+++ vue-application/src/components/Test.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>{{ msg }}</h1>
+</template>
+
+<script>
+export default {
+  name: "HelloVie",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<style>
+</style>
