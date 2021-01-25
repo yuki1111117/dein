66266dce95e9f68238bb3fec4ea1b8dd16f77678
Your Name <you@example.com>
2021-01-25 18:14:51 +0900

    use eslint

--- dein/src/App.vue
+++ dein/src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Welcome to Your Vue.js App" />
+  <Test />
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
+import Test from "./components/Test.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+    HelloWorld,
+    Test
   }
 };
 </script>
---
+++ dein/src/components/Test.vue
@@ -0,0 +1,38 @@
+<template lang="">
+  <div id="overlay">
+    <div id="content">
+      <p>これがモーダルウィンドウです。</p>
+      <p><button>close</button></p>
+    </div>
+  </div>
+  >
+</template>
+<script>
+import axios from "axios";
+export default {
+  name: "Test",
+  //API格納用のプロパティ
+  data: function() {
+    return {
+      bpi: null
+    };
+  },
+
+  //axiosによるデータ取得処理
+  mounted: function() {
+    axios
+      .get("https://api.coindesk.com/v1/bpi/currentprice.json")
+      .then(
+        function(response) {
+          //デバッグ用にconsoleに出力
+          console.log(response.data.bpi);
+          this.bpi = response.data.bpi;
+        }.bind(this)
+      )
+      .catch(function(error) {
+        console.log(error);
+      });
+  }
+};
+</script>
+<style lang=""></style>
